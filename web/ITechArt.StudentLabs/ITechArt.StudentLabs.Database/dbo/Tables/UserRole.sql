29b19036eef36921e49f2cc391baaa623cc53fde
Hanna Baryskina <boriskina.anna99@gmail.com>
2018-07-26 17:24:45 +0300

    code refactor

--- web/ITechArt.StudentLabs/ITechArt.StudentLabs.Database/dbo/Tables/UserRole.sql
+++ web/ITechArt.StudentLabs/ITechArt.StudentLabs.Database/dbo/Tables/UserRole.sql
@@ -5,6 +5,6 @@
     [RoleId] INT NOT NULL,
     CONSTRAINT [PK_UserRole] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_UserRole_Role] FOREIGN KEY([RoleId]) REFERENCES [dbo].[Role] ([Id]),
-	CONSTRAINT [FK_UserRole_User] FOREIGN KEY([UserId]) REFERENCES [dbo].[User] ([Id]),
+    CONSTRAINT [FK_UserRole_User] FOREIGN KEY([UserId]) REFERENCES [dbo].[User] ([Id]),
     CONSTRAINT [UX_UserRole_UserId_RoleId] UNIQUE NONCLUSTERED ([RoleId] ASC, [UserId] ASC)
 );
